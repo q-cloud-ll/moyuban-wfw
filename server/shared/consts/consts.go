ce15d519c174361df5b6caacf1dfe111dde150f0
qcsj <cherryqcsj@gmail.com>
2023-11-07 10:52:16 +0800

    ip,port func

---
+++ server/shared/consts/consts.go
@@ -0,0 +1,12 @@
+package consts
+
+const (
+	Moyuban = "Moyuban"
+
+	HlogFilePath = "./tmp/hlog/logs/"
+	KlogFilePath = "./tmp/klog/logs/"
+
+	TCP             = "tcp"
+	FreePortAddress = "localhost:0"
+	CorsAddress     = "http://localhost:3000"
+)
